3c249c938fa48b0f766fe7e55996ce0747123756
Roman Tuchin <roman.tuchin@idealo.de>
2016-08-21 13:44:08 +0200

    add external logging configuration

--- src/DaemonApplication.cpp
+++ src/DaemonApplication.cpp
@@ -5,8 +5,10 @@
 #include <string>
 #include <vector>
 #include "Poco/Util/TimerTaskAdapter.h"
+#include "Poco/Logger.h"
 #include "partymaker/DaemonApplication.h"
 #include "partymaker/DownloadMeasureTask.h"
+
 
 
 
@@ -15,12 +17,17 @@ using Poco::Thread;
 using std::string;
 using Poco::Util::TimerTask;
 using Poco::Util::TimerTaskAdapter;
+using Poco::Logger;
+using Poco::Util::ServerApplication;
 
 DaemonApplication::DaemonApplication() {
   logger().information("ctor calling...");
 }
 
 void DaemonApplication::initialize(Application &self) {
+  loadConfiguration("/data/partymaker/conf/partymaker.properties");
+  // log config is read only with next line
+  ServerApplication::initialize(self);
   logger().information("init sheduled tasks");
   DownloadMeasureTask task;
   TimerTask::Ptr ptr = new TimerTaskAdapter<DownloadMeasureTask>(
